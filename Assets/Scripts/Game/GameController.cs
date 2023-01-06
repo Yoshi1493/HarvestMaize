5fd3e491a1b7fcd8273c8dcec61b965ac788980a
Nic1493 <nic.choy107@gmail.com>
2023-01-06 16:47:03 -0500

    Impl. basic player movement

---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+}
--- Assets/Scripts/Scriptable Objects/CharacterMovementData.cs
+++ Assets/Scripts/Scriptable Objects/CharacterMovementData.cs
@@ -3,18 +3,18 @@ using UnityEngine;
 [CreateAssetMenu]
 public class CharacterMovementData : ScriptableObject
 {
-    public Vector2 gravity = new(0, -9.8f);
+    public Vector2 gravity = new(0, -49f);
 
     [Space]
 
-    public float moveSpeed = 10f;
-    public float jumpHeight = 12f;
+    public float moveSpeed = 8f;
+    public float jumpHeight = 20f;
     public float variableJumpHeightMultiplier = 0.5f;
 
     [Space]
 
-    public float groundedDamping = 20f;
-    public float airborneDamping = 8f;
+    public float groundedDamping = 0.05f;
+    public float airborneDamping = 0.10f;
 
     [Space]
 
