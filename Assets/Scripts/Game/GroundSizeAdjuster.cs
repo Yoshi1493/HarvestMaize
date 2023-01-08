f543b7a8e41f36164ee4e01da69c67511a04cdf5
Yoshi1493 <nic.choy107@gmail.com>
2023-01-08 18:42:52 -0500

    Impl. ground size adjustment based on selected difficulty

--- Assets/Scripts/Game/GroundSizeAdjuster.cs
+++ Assets/Scripts/Game/GroundSizeAdjuster.cs
@@ -1,10 +1,18 @@
-using System.Collections;
 using UnityEngine;
 
 public class GroundSizeAdjuster : MonoBehaviour
 {
-    void Awake()
+    [SerializeField] IntObject selectDifficulty;
+    [SerializeField] Vector2IntObject[] mazeDimensionObjects;
+
+    const float ScaleFactor = 0.02f;
+
+    void Start()
     {
-        
+        Vector2Int mazeDimensions = mazeDimensionObjects[selectDifficulty.value].value;
+        float x = mazeDimensions.y * ScaleFactor;
+        float y = mazeDimensions.x * ScaleFactor;
+
+        transform.localScale = new(x, y, 1);
     }
 }
