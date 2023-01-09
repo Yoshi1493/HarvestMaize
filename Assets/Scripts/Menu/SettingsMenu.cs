84faaf6b15f07d3f9cfd247501f6b45148c0c6d0
Yoshi1493 <nic.choy107@gmail.com>
2023-01-08 19:23:20 -0500

    Update settings menu to choose map size

--- Assets/Scripts/Menu/SettingsMenu.cs
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -1,10 +1,23 @@
+using System;
+using UnityEngine;
+using TMPro;
+
 public class SettingsMenu : Menu
 {
+    int maxMapDimensions;
+    [SerializeField] GameObject[] mapDimensionChangeButtons;
+    [SerializeField] TextMeshProUGUI mapDimensionText;
+
+    [SerializeField] IntObject selectedDifficulty;
+
     PauseHandler pauseHandler;
 
     protected override void Awake()
     {
         base.Awake();
+
+        maxMapDimensions = Enum.GetValues(typeof(MazeSize)).Length;
+        UpdateUIElements();
 
         pauseHandler = FindObjectOfType<PauseHandler>();
         if (pauseHandler != null)
@@ -19,5 +32,19 @@ public class SettingsMenu : Menu
         {
             Close();
         }
+    }
+
+    void UpdateUIElements()
+    {
+        mapDimensionText.text = ((MazeSize)selectedDifficulty.value).ToString();
+
+        mapDimensionChangeButtons[0].SetActive(selectedDifficulty.value > 0);
+        mapDimensionChangeButtons[1].SetActive(selectedDifficulty.value < maxMapDimensions - 1);
+    }
+
+    public void OnChangeMapDimensions(int amount)
+    {
+        selectedDifficulty.value = Mathf.Clamp(selectedDifficulty.value + amount, 0, maxMapDimensions - 1);
+        UpdateUIElements();
     }
 }
