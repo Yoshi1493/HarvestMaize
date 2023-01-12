4174f3f1a9f96721f05bbd6ba1c591914aa3d80b
Yoshi1493 <nic.choy107@gmail.com>
2023-01-12 00:12:18 -0500

    add Player slash effect
    created PlayerSlashEffect shader, material, and script to handle displaying slash effects upon harvesting a crop

---
+++ Assets/Scripts/Game/Actors/Player/PlayerSlashEffect.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+using static CoroutineHelper;
+
+public class PlayerSlashEffect : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    Material slashMaterial;
+
+    IEnumerator slashAnimationCoroutine;
+    float animationSpeed;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        spriteRenderer.enabled = false;
+
+        slashMaterial = spriteRenderer.material;
+        slashMaterial.SetFloat("_AnimationTime", 0f);
+        animationSpeed = slashMaterial.GetFloat("_AnimationSpeed");
+
+        GetComponentInParent<PlayerHarvester>().HarvestAction += () => enabled = true;
+    }
+
+    void OnEnable()
+    {
+        if (slashAnimationCoroutine != null)
+        {
+            StopCoroutine(slashAnimationCoroutine);
+        }
+
+        slashAnimationCoroutine = AnimateSlash();
+        StartCoroutine(slashAnimationCoroutine);
+    }
+
+    IEnumerator AnimateSlash()
+    {
+        float currentLerpTime = 0f;
+        float totalLerpTime = 1 / animationSpeed;
+
+        spriteRenderer.enabled = true;
+
+        while (currentLerpTime < totalLerpTime)
+        {
+            float lerpProgress = currentLerpTime / totalLerpTime;
+            float offset = Mathf.Lerp(0f, 1f, lerpProgress);
+            slashMaterial.SetFloat("_AnimationTime", offset);
+
+            yield return EndOfFrame;
+            currentLerpTime += Time.deltaTime;
+        }
+
+        spriteRenderer.enabled = false;
+        enabled = false;
+    }
+}
